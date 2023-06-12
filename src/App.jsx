e2eaa718560951e79a29155e6f620bc98805c7fb
Natalia Burdyey <nata.burdey@gmail.com>
2023-06-12 18:24:17 -0400

    Color Generator complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+
+import ColorList from './ColorList';
+import Form from './Form';
+import Values from 'values.js';
+
 const App = () => {
-  return <h2>Color Generator</h2>;
+  const [colors, setColors] = useState(new Values('#f15025').all(10));
+
+  const addColor = (color) => {
+    try {
+      const newColors = new Values(color).all(10);
+      setColors(newColors);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <main>
+      <Form addColor={addColor} />
+      <ColorList colors={colors} />
+      <ToastContainer position='top-center' />
+    </main>
+  );
 };
 export default App;
